ed4e04391a0c6d1954777133485db6018c6a7368
binod karki <ibinod3676@gmail.com>
2018-02-20 12:21:36 -0500

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { PosterComponent } from './poster/poster.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PosterComponent,
+    HomeComponent,
+    NavbarComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule
