3611f5cb17269609e55ee563753877536dd76a0f
binod karki <ibinod3676@gmail.com>
2018-02-23 00:08:10 -0500

    tested

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { FormsModule } from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
 import { PosterComponent } from './poster/poster.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SignupComponent } from './signup/signup.component';
+import { UploadComponent } from './upload/upload.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
-
-const appRoutes: Routes = [
+ const appRoutes: Routes = [
   {
 path: 'signup', component: SignupComponent},
 { path: '', redirectTo: '/signup', pathMatch: 'full'}
@@ -24,11 +25,15 @@ path: 'signup', component: SignupComponent},
     PosterComponent,
     HomeComponent,
     NavbarComponent,
-    SignupComponent
+    SignupComponent,
+    UploadComponent,
+    HeroesComponent,
+ 
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    FormsModule,
+    RouterModule.forRoot(appRoutes,{ enableTracing: true  })
   ],
   providers: [],
   bootstrap: [AppComponent]
